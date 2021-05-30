0434cb801b1a12def08b5abe1d36c56d00c93f92
Remy Bouvard <remy.bouvard@epitech.eu>
2021-05-30 16:06:28 +0200

    [add] add documentation swagger for api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const appOptions = {cors: true};
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const config = new DocumentBuilder()
+  .setTitle('Chinese Checker')
+  .setDescription('The chinese checker API documentation')
+  .setVersion('1.0')
+  .addTag('Users')
+  .build()
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
